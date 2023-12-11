ca1c0c0b86dee9f76f046264f0e4bb0c46830669
rbadvaith <rb.advaith@gmail.com>
2023-12-11 11:35:50 -0500

    commit

---
+++ estore/store/urls.py
@@ -0,0 +1,15 @@
+from django.urls import path
+
+from . import views
+
+urlpatterns = [
+	
+	path('', views.store, name="store"),
+	path('cart/', views.cart, name="cart"),
+	path('checkout/', views.checkout, name="checkout"),
+    path('search/', views.product_search, name='product_search'),
+
+	path('update_item/', views.updateItem, name="update_item"),
+	path('process_order/', views.processOrder, name="process_order"),
+
+]
